b05f4df1419e59dc306e83255b68f75121bcb3c0
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2023-02-04 23:27:28 +0100

    Added command line

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,73 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fs::File, io::Write};
+
+#[derive(Parser, Debug)]
+#[command(author, version)]
+struct Args {
+    /// Path to the xochit binary
+    binary_path: String,
+
+    /// Version number from /etc/version
+    version_number: u64,
+
+    /// Version string from GUI
+    version_string: String,
+
+    /// Output location
+    #[arg(short, default_value = "./rm2fb.conf")]
+    output: String,
+}
 
 #[macro_use]
 extern crate r2pipe;
 extern crate serde_json;
+use clap::Parser;
 use r2pipe::R2Pipe;
 fn main() {
-    let path = Some("./xochitl");
+    let args = Args::parse();
+
+    let path = Some(args.binary_path);
 
     let mut r2p = open_pipe!(path).unwrap();
 
     r2p.cmd("aaaa").unwrap();
     r2p.cmd("aae").unwrap();
 
+    let mut out_file = File::create(args.output).unwrap();
+
+    out_file
+        .write(
+            format!(
+                "!{}\nversion str {}\n",
+                args.version_number, args.version_string
+            )
+            .as_bytes(),
+        )
+        .unwrap();
+
     // update
     let update_fn = r2p
         .cmdj("axtj @str.Unable_to_complete_update:_invalid_waveform__")
         .unwrap();
     let update_addr = update_fn[0]["fcn_addr"].as_u64().unwrap();
-    println!("update addr {:#01x}", update_addr);
+    out_file
+        .write(format!("update addr {:#01x}\nupdateType str QRect\n", update_addr).as_bytes())
+        .unwrap();
 
     // create
     let create_fn = r2p
         .cmdj("axtj @str.Unable_to_start_generator_thread")
         .unwrap();
     let create_addr = create_fn[0]["fcn_addr"].as_u64().unwrap();
-    println!("create addr {:#01x}", create_addr);
+    out_file
+        .write(format!("create addr {:#01x}\n", create_addr).as_bytes())
+        .unwrap();
 
     // shutdown
     let shutdown_fn = r2p.cmdj("axtj @str.Shutting_down...").unwrap();
     let shutdown_addr = shutdown_fn[0]["fcn_addr"].as_u64().unwrap();
-    println!("shutdown addr {:#01x}", shutdown_addr);
+    out_file
+        .write(format!("shutdown addr {:#01x}\n", shutdown_addr).as_bytes())
+        .unwrap();
 
     // notify
     r2p.cmd(format!("s {update_addr}").as_str()).unwrap();
@@ -53,7 +91,9 @@ fn main() {
         });
 
     let (notify_addr, _) = fn_map.iter().find(|(_, &c)| c == 2).unwrap();
-    println!("notify addr {:#01x}", notify_addr);
+    out_file
+        .write(format!("notify addr {:#01x}\n", notify_addr).as_bytes())
+        .unwrap();
 
     // wait
     let imports = r2p.cmdj("iij").unwrap();
@@ -87,7 +127,9 @@ fn main() {
 
     let (wait_addr, _) = fn_map.iter().max_by(|(_, a), (_, b)| a.cmp(b)).unwrap();
 
-    println!("wait addr {:#01x}", wait_addr);
+    out_file
+        .write(format!("wait addr {:#01x}\n", wait_addr).as_bytes())
+        .unwrap();
 
     // getInstance
     let wait_callers = r2p.cmdj(format!("axtj {wait_addr}").as_str()).unwrap();
@@ -109,7 +151,9 @@ fn main() {
 
     let get_instance_addr = callers[0]["fcn_addr"].as_u64().unwrap();
 
-    println!("getInstance addr {:#01x}", get_instance_addr);
+    out_file
+        .write(format!("getInstance addr {:#01x}\n", get_instance_addr).as_bytes())
+        .unwrap();
 
     r2p.close();
 }
